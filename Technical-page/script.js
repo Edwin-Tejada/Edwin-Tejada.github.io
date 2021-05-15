2a745725a5ec43f560aedb01571c303915a72b0a
Edwin Tejada <edwintejadav@gmail.com>
2021-05-15 10:32:37 -0600

    Add JS

---
+++ Technical-page/script.js
@@ -0,0 +1,26 @@
+//AOS Animation on Scroll
+AOS.init({
+  offset: 30,
+  duration: 1300,
+  easing: 'ease-in-out',
+  delay: 70,
+});
+
+
+//Responsive Nav Mobile
+const header = document.getElementById('header')
+const toggle = document.getElementById('toggle');
+const navbar = document.getElementById('navbar');
+
+      document.onclick = function(e){
+        if(e.target.id !== 'header' && e.target.id !== 'toggle' && e.target.id !== 'navbar'){
+          toggle.classList.remove('active');
+          navbar.classList.remove('active');
+        }
+      }
+
+      toggle.onclick = function(){
+        toggle.classList.toggle('active');
+        navbar.classList.toggle('active');
+      }
+      
